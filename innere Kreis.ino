faadc1d9ccd8ca9a437318c541426e8ae219a157
Dype-1 <dlud2275@gmail.com>
2025-11-24 08:16:51 +0100

    innere Kreis.ino

--- innere Kreis.ino
+++ innere Kreis.ino
@@ -1,7 +1,13 @@
-// soll, ist und istGeschwindigkeit existieren bereits!
-// hier wird NUR die neue Geschwindigkeit berechnet
+float diff = soll - ist;
 
-float faktor = 0.2 + 0.05 * (abs(soll - ist) / 100.0);
+float faktor = 0.2 + 0.05 * (abs(diff) / 100.0);
 faktor = constrain(faktor, 0.2, 0.7);
 
-float speed = faktor * 40;   // das ist deine neue Geschwindigkeit
+float speedValue = faktor * 40;
+
+if (diff == 0) {
+    speed = 0;
+} else {
+    speed = (diff > 0) ? speedValue : -speedValue;
+}
+
