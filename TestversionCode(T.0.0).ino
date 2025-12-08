fa890da3cbf5c2032323bdd1ecf7a004d7905ed9
Xeno268 <sebastian.a.merz@gmail.com>
2025-12-08 07:50:14 +0100

    Update TestversionCode(T.0.0).ino

--- TestversionCode(T.0.0).ino
+++ TestversionCode(T.0.0).ino
@@ -1,2 +1,13 @@
 #include <Encoder.h>
 
+String empfangen = "a";   
+String erweitert = empfangen + "b";  
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(erweitert);  
+}
+
+void loop() {
+}
+
