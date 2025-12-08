c88d7e4d1f006559755f9c9ced8bc9c526f3e235
EBVIOO <ebvakoeuwaifo@gmail.com>
2025-12-08 08:22:35 +0100

    Simplify loop and clean up code
    Refactor loop function and remove unused functions

--- TestversionCode(T.0.0).ino
+++ TestversionCode(T.0.0).ino
@@ -18,29 +18,15 @@ void setup() {
 void loop() {
   String Sebastian = "B";                    
   String SebastianErgebnis = Julian() + Sebastian;
+
   Serial.println(SebastianErgebnis);         
   delay(1000);                               
-}
-
-
-void setup() {
-}
-void loop(){
-String AB = Sebastian(Julian());
-  
 }
 
 String Julian() {
   String A = "A";
   return A;
-}
 
-String Sebastian (String A) {
-}
-
-String Abdul_One() {
-  String One = "D";
-  return One;
 
 String setSollwertInnereRegelung(String A_B,String C) {
   String ABC =(A_B - C);
