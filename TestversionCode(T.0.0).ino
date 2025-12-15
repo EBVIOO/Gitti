4d99a463bc921720c2ca9b30e2794af83c567274
EBVIOO <ebvakoeuwaifo@gmail.com>
2025-12-15 08:44:27 +0100

    Implement encoder reading and speed calculation
    Added encoder functionality to read positions and calculate speeds.

--- TestversionCode(T.0.0).ino
+++ TestversionCode(T.0.0).ino
@@ -1,19 +1,37 @@
-String x = "x";
-String Y = "Y";
+#include <Encoder.h>
 
+Encoder myEnc1(5, 6);
+Encoder myEnc2(7, 8);
 
+const float Zeit = 0.001; // Abtastzeit [s]
+
+long pos1_alt = 0;
+long pos2_alt = 0;
+
+long pos1, pos2;
+float sI1, sI2;
 
 void setup() {
-  Serial.begin(9600); // Serial Monitor starten
+  Serial.begin(9600);
 }
 
 void loop() {
-  
-                 
+  // Positionswerte auslesen
+  pos1 = myEnc1.read();
+  pos2 = myEnc2.read();
+
+  // Geschwindigkeit = Differenz der Counts / Zeit
+  sI1 = (pos1 - pos1_alt) / Zeit;
+  sI2 = (pos2 - pos2_alt) / Zeit;
+
+  // Alte Position speichern
+  pos1_alt = pos1;
+  pos2_alt = pos2;
+
+
+
+
 }
-
-
-
 void setSollwertAussereRegelung(float as1,float as2,float aI1,float aI2) {
   float sS1 = as1 -aI1
   float sS2 = as2 - aI2
