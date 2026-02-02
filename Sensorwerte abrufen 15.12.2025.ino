a26ac06b348b039c343f5b89f1db3be08f004654
EBVIOO <ebvakoeuwaifo@gmail.com>
2026-02-02 08:55:15 +0100

    Refactor speed calculation and add time tracking
    Updated the calculation of speed and added time tracking.

--- Sensorwerte abrufen 15.12.2025.ino
+++ Sensorwerte abrufen 15.12.2025.ino
@@ -3,26 +3,50 @@
 Encoder myEnc1(5, 6);
 Encoder myEnc2(7, 8);
 
-const float Zeit = 0.001; // Abtastzeit [s]
+
 
 long pos1_alt = 0;
 long pos2_alt = 0;
 
 long pos1, pos2;
+float aI1, aI2;
 float sI1, sI2;
+float t1 = 0;
+float t2 = 0;
+
+unsigned long Zeit;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
+  
+  Zeit = millis()
+  
   // Positionswerte auslesen
   pos1 = myEnc1.read();
   pos2 = myEnc2.read();
 
-  // Geschwindigkeit = Differenz der Counts / Zeit
-  sI1 = (pos1 - pos1_alt) / Zeit;
-  sI2 = (pos2 - pos2_alt) / Zeit;
+  aI1 = (pos1 - pos1_alt)/2;
+  aI2 = (pos2 - pos2_alt)/2;
+
+  if (pos1_alt !=  pos1){
+    t1 = t2;
+    t2 = millis();
+    Zeit = t2 -t1;
+
+  }
+
+
+
+  // WinkelGeschwindigkeit = Differenz der Counts / Zeit
+  sI1 = (aI1) / Zeit;
+  sI2 = (aI2) / Zeit;
+
+
+
+  
 
   // Alte Position speichern
   pos1_alt = pos1;
@@ -36,3 +60,4 @@ void loop() {
 
   delay(200);
 }
+
